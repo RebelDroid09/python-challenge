19473db6eaffc9262145607ac77014b83839aad5
RebelDroid09 <paulcs91@gmail.com>
2020-09-20 22:48:16 -0500

    Updated code after testing
    Corrected some issues with the code, and added the start of the code for printing the final results.

--- PyBank/bankScript.py
+++ PyBank/bankScript.py
@@ -1,26 +1,42 @@
 import csv
+import os
 
-with open('../Resources/budget_data.csv', newline='') as csvfile:
+csvPath = os.path.join(".", "Resources", "budget_data.csv")
+
+with open(csvPath) as csvfile:
     lineReader = csv.reader(csvfile, delimiter=',')
 
     lineCount = 0
     runningTotal = 0
     average = 0
     greatestIncrease = 0
-    greatestDecrease = 0
+    greatestDecrease = 0   
 
     for row in lineReader:
-        if lineCount == 0:
-            pass
-        else:
-            if isnumeric(row[1]):
-                runningTotal += row[1]
+        if lineCount > 0:
+            try:
+                convertedValue = int(row[1])
+                runningTotal += convertedValue
 
-                if row[1] > greatestIncrease:
-                    greatestIncrease = row[1]
-                elif row[1] < greatestDecrease:
-                    greatestDecrease = row[1]
+                print(convertedValue)
 
-            lineCount += 1
+                if convertedValue > greatestIncrease:
+                    greatestIncrease = convertedValue
+                elif convertedValue < greatestDecrease:
+                    greatestDecrease = convertedValue     
+            except ValueError:
+                pass     
+        print(row)
+        lineCount += 1
 
-average = runningTotal / lineCount
+if lineCount > 0:
+    average = runningTotal / (lineCount - 1)
+
+print(lineCount)
+print(runningTotal)
+print(average)
+print(greatestIncrease)
+print(greatestDecrease)
+
+finalText = "Total Months: {months} Total: ${total} Average Change: ${average}2 Greatest Increase in Profits: {highestMonth} (${highestAmount}) Greatest Decrease in Profits: {lowestMonth} (${lowestMonth}) "
+print()
