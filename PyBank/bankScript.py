94228355b27ffcd0c5287f37444615d5a562be78
RebelDroid09 <paulcs91@gmail.com>
2020-09-21 20:10:26 -0500

    Final script changes
    Finished final testing, and removed extra debugging print statements.

--- PyBank/bankScript.py
+++ PyBank/bankScript.py
@@ -10,7 +10,9 @@ with open(csvPath) as csvfile:
     runningTotal = 0
     average = 0
     greatestIncrease = 0
-    greatestDecrease = 0   
+    greatestDecrease = 0  
+    highestMonth = ""
+    lowestMonth = "" 
 
     for row in lineReader:
         if lineCount > 0:
@@ -18,25 +20,22 @@ with open(csvPath) as csvfile:
                 convertedValue = int(row[1])
                 runningTotal += convertedValue
 
-                print(convertedValue)
-
                 if convertedValue > greatestIncrease:
                     greatestIncrease = convertedValue
+                    highestMonth = row[0]
                 elif convertedValue < greatestDecrease:
                     greatestDecrease = convertedValue     
+                    lowestMonth = row[0]
             except ValueError:
                 pass     
-        print(row)
+       
         lineCount += 1
 
 if lineCount > 0:
     average = runningTotal / (lineCount - 1)
 
-print(lineCount)
-print(runningTotal)
-print(average)
-print(greatestIncrease)
-print(greatestDecrease)
+finalText = "Total Months: {months} Total: ${total} Average Change: ${average:.2f} Greatest Increase in Profits: {highestMonth} (${highestAmount}) Greatest Decrease in Profits: {lowestMonth} (${lowestAmount})"
 
-finalText = "Total Months: {months} Total: ${total} Average Change: ${average}2 Greatest Increase in Profits: {highestMonth} (${highestAmount}) Greatest Decrease in Profits: {lowestMonth} (${lowestMonth}) "
-print()
+print("Financial Analysis")
+print("-" * 20)
+print(finalText.format(months = lineCount, total = runningTotal, average = average, highestMonth = highestMonth, highestAmount = greatestIncrease, lowestMonth = lowestMonth, lowestAmount = greatestDecrease))
